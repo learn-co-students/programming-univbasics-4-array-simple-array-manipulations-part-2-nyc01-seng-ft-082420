8418d4a8499312d93742fc95e7ab579fce5652d7
Ming Lin <minglianglin50@gmail.com>
2020-08-08 18:49:24 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(arr1, arr2)
+  return arr1.concat(arr2)
+end
+
+def using_insert(arr, el)
+  return arr.insert(4, el)
+end
+
+def using_uniq(arr)
+  return arr.uniq()
+end
+
+def using_flatten(arr)
+  return arr.flatten()
+end
+
+def using_delete(arr, str)
+  return arr.delete(str)
+end
+
+def using_delete_at(arr, int)
+  return arr.delete_at(int)
+end
