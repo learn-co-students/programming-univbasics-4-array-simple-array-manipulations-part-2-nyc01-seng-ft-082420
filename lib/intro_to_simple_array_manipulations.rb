9360540fbb193db3151058c694fb7dd54eb721d1
mamadou alpha barry <mamadou.barry1105@gmail.com>
2020-08-14 18:25:13 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_concat(arr, x)
+  arr.concat(x)
+end
+
+def using_insert(arr, x)
+  arr.insert(4, x)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, s)
+  arr.delete(s)
+end
+
+def using_delete_at(arr, i)
+  arr.delete_at(i)
+end
+
